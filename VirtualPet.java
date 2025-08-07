2da59f6d27720e7ba62c9c4f066d1e1a070b1e67
Joshua-Melvin-2024-aia <melvinjoshua777@gmail.com>
2025-08-07 11:37:13 +0530

    Main.java

---
+++ VirtualPet.java
@@ -0,0 +1,42 @@
+// VirtualPet.java
+
+class VirtualPet {
+    // Member variables
+    String name;
+    int energy;
+    int happiness;
+
+    // Constructor
+    VirtualPet(String petName) {
+        name = petName;
+        energy = 50;
+        happiness = 50;
+    }
+
+    // Member functions (methods)
+    void feed() {
+        energy += 10;
+        System.out.println(name + " has been fed. Energy is now " + energy);
+    }
+
+    void play() {
+        if (energy >= 10) {
+            happiness += 15;
+            energy -= 10;
+            System.out.println(name + " played and is happy! Happiness: " + happiness + ", Energy: " + energy);
+        } else {
+            System.out.println(name + " is too tired to play.");
+        }
+    }
+
+    void sleep() {
+        energy += 20;
+        System.out.println(name + " is sleeping... Energy restored to " + energy);
+    }
+
+    void showStatus() {
+        System.out.println("\n--- " + name + "'s Status ---");
+        System.out.println("Energy: " + energy);
+        System.out.println("Happiness: " + happiness);
+    }
+}
